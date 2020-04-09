47556abfb9ac5fa47106a8ed854edea0d3a22463
André Vieira <sgavsnake@gmail.com>
2020-04-09 17:10:15 -0300

    remove token...

--- src/main/java/services/config/ServiceParams.java
+++ src/main/java/services/config/ServiceParams.java
@@ -2,5 +2,5 @@ package services.config;
 
 public class ServiceParams {
     public static String BASE_URL = "https://api.codenation.dev/v1/challenge/dev-ps/";
-    public static String TOKEN = "a9c53a670783fd29e561700b53c63d5d8ebd5fda";
+    public static String TOKEN = "";
 }
